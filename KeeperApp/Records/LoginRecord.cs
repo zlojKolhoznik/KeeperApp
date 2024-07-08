6f91b51e241a60b2f0300ebc7834f260a421fbf5
Roman Portianko <rportyanko10@gmail.com>
2024-07-08 11:19:58 +0300

    Added first type of record - login

---
+++ KeeperApp/Records/LoginRecord.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+
+namespace KeeperApp.Records
+{
+    public class LoginRecord : Record, IParsable<LoginRecord>
+    {
+        private string login;
+        private string password;
+        private string url;
+        private string notes;
+
+        public string Login 
+        { 
+            get => Decrypt<string>(login);
+            set
+            {
+                login = Encrypt(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => Decrypt<string>(password);
+            set
+            {
+                password = Encrypt(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public string Url
+        {
+            get => Decrypt<string>(url);
+            set
+            {
+                url = Encrypt(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public string Notes
+        {
+            get => Decrypt<string>(notes);
+            set
+            {
+                notes = Encrypt(value);
+                OnPropertyChanged();
+            }
+        }
+
+        public static LoginRecord Parse(string s, IFormatProvider provider)
+        {
+            return JsonSerializer.Deserialize<LoginRecord>(s);
+        }
+
+        public static LoginRecord Parse(string s)
+        {
+            return Parse(s, null);
+        }
+
+        public static bool TryParse([NotNullWhen(true)] string s, IFormatProvider provider, [MaybeNullWhen(false)] out LoginRecord result)
+        {
+            bool returnValue;
+            try
+            {
+                result = Parse(s, provider);
+                returnValue = true;
+            }
+            catch
+            {
+                result = null;
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ KeeperApp/Records/Record.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeeperApp.Records;
+
+public class Record : ObservableObject
+{
+    public int Id { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+    public bool IsDeleted { get; set; }
+    protected byte[] IV { get; set; }
+
+    public Record()
+    {
+        Created = DateTime.Now;
+        Modified = DateTime.Now;
+        IsDeleted = false;
+    }
+
+    protected T Decrypt<T>(string value) where T : IParsable<T>
+    {
+        var key = GetEncryptionKey();
+        using var aes = Aes.Create();
+        var decryptor = aes.CreateDecryptor(key, IV);
+        var bytes = Convert.FromBase64String(value);
+        var decryptedBytes = decryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+        return T.Parse(Encoding.UTF8.GetString(decryptedBytes), null);
+    }
+
+    protected string Encrypt<T>(T value) where T : IParsable<T>
+    {
+        var key = GetEncryptionKey();
+        using var aes = Aes.Create();
+        if (IV is null or { Length: 0 })
+        {
+            IV = aes.IV;
+        }
+        var encryptor = aes.CreateEncryptor(key, IV);
+        var bytes = Encoding.UTF8.GetBytes(value.ToString());
+        var encryptedBytes = encryptor.TransformFinalBlock(bytes, 0, bytes.Length);
+        return Convert.ToBase64String(encryptedBytes);
+    }
+
+    private byte[] GetEncryptionKey()
+    {
+        var bytes = Encoding.UTF8.GetBytes(Created.ToString());
+        return SHA256.HashData(bytes);
+    }
+}
